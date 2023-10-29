5d8ced8add7806e254ca172d898d958598fbc582
newdlops <newdlops@gmail.com>
2023-10-29 19:00:57 +0900

    web url로 수정

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,2 +1,3 @@
-export const serverUri = `http://localhost:3000`
+export const serverUri = `https://api.quickc.co.kr`
+// export const serverUri = `http://localhost:3000`
 // export const serverUri = `http://10.0.2.2:3000`
