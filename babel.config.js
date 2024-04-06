398f1426ff844e4728737a24af98300e01650f03
newdlops <newdlops@gmail.com>
2024-04-07 02:16:50 +0900

    deploy web

--- babel.config.js
+++ babel.config.js
@@ -1,40 +1,42 @@
-module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    '@babel/preset-typescript',
-  ],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        extensions: [
-          '.ios.ts',
-          '.android.ts',
-          '.web.ts',
-          '.ts',
-          '.ios.tsx',
-          '.android.tsx',
-          '.web.ts',
-          '.tsx',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-        alias: {
-          '~': './src',
-          '@components': './src/components',
-          '@navigators': './src/navigators',
-          '@screens': './src/screens',
-          '@store': './src/store',
-          '@utils': './src/utils',
-          // '^react-native$': 'react-native-web',
-          // 웹빌드가 아닐때는 주석처리한다(react-native를 모두 react-native로 대체함
-          // TODO 웹, 앱 설정파일을 별도로 만들어서 자동화
+module.exports = function(api) {
+  api.cache(true)
+  const isweb = process.env.IS_WEB
+  const alias = {
+    '~': './src',
+    '@components': './src/components',
+    '@navigators': './src/navigators',
+    '@screens': './src/screens',
+    '@store': './src/store',
+    '@utils': './src/utils',
+  }
+  if(isweb){
+    alias['^react-native$'] = 'react-native-web'
+  }
+  return {
+    presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.web.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.web.ts',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: alias,
         },
-      },
+      ],
+      '@babel/plugin-proposal-export-namespace-from', // reanimated web
+      'react-native-reanimated/plugin', // 항상 가장 마지막에 와야함
     ],
-    '@babel/plugin-proposal-export-namespace-from', // reanimated web
-    'react-native-reanimated/plugin', // 항상 가장 마지막에 와야함
-  ],
+  }
 }
