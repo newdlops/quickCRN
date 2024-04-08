cc7e7ee96251ef088a28f75db7d5aa81ac6b5a4e
newdlops <newdlops@gmail.com>
2024-04-08 09:10:21 +0900

    server uri

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,9 @@
 import { Platform } from 'react-native'
 
-// export const serverUri = `https://api.quickc.co.kr`
-export const normalUri = `http://localhost:3000`
+export const serverUri = `https://api.quickc.co.kr`
+// export const normalUri = `http://localhost:3000`
 // export const serverUri = `https://metal-rockets-run.loca.lt`
-export const androidUri = `http://10.0.2.2:3000`
+// export const androidUri = `http://10.0.2.2:3000`
 // export const androidUri = `https://rotten-falcons-warn.loca.lt`
 
-export const serverUri = Platform.OS == 'android' ? androidUri:normalUri
+// export const serverUri = Platform.OS == 'android' ? androidUri:normalUri
